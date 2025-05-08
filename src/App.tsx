7e026d373542386a72d33ef1e1c3d35197971370
Patrícia Silva <psilva@imaginarycloud.com>
2025-05-08 18:05:49 +0100

    add store provider to app + test out action dispatch to populate questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
+import { RootState } from "./store";
+import { setQuestions } from "./store/quizSlice";
 import viteLogo from "/vite.svg";
 
+const initialQuestion1to5: Q1to5 = {
+  format: "1to5",
+  options: [
+    { imageUrl: "https://via.placeholder.com/150" },
+    { imageUrl: "https://via.placeholder.com/150" },
+    { imageUrl: "https://via.placeholder.com/150" },
+  ],
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
+  const questions = useSelector((state: RootState) => state.quiz.questions);
+  const index = useSelector((state: RootState) => state.quiz.currentIndex);
+  const currentQuestion = questions[index];
+
+  const setQuestionSet = () => {
+    dispatch(setQuestions([initialQuestion1to5]));
+  };
 
   return (
     <>
@@ -18,12 +36,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={setQuestionSet}>set questions</button>
+        <p>{questions.length} Questions saved</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
