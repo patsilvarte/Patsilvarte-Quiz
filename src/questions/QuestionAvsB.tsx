47b67c6ec6d86b267999b0df4f2003303bfa635d
Patrícia Silva <psilva@imaginarycloud.com>
2025-05-13 00:50:53 +0100

    fix styling on AvsB

--- src/questions/QuestionAvsB.tsx
+++ src/questions/QuestionAvsB.tsx
@@ -1,4 +1,5 @@
 import { Card, CardMedia, Grid, Typography } from "@mui/material";
+import { CSSProperties } from "react";
 import { useDispatch } from "react-redux";
 import { answerQuestion } from "../store/quizSlice";
 
@@ -10,31 +11,57 @@ export const QuestionAvsB: React.FC<QuestionCardProps> = ({ question }) => {
   const dispatch = useDispatch();
 
   const { image1, image2, pick } = question;
-  const isPicked = (val: string) => pick === val;
 
   const onChange = (value: string) => {
-    const updated: QAvsB = { ...question, pick: value };
+    const isPicked = pick === value;
+    const updated: QAvsB = { ...question };
+    if (isPicked) {
+      updated.pick = undefined;
+    } else {
+      updated.pick = value;
+    }
     dispatch(answerQuestion({ updated }));
+  };
+
+  const getStyle = (value: string) => {
+    const isPicked = pick === value;
+    if (!pick) {
+      return {} as CSSProperties;
+    }
+    if (isPicked) {
+      return {
+        borderColor: "#535931",
+        // scale: 1.2,
+        transform: "scale(1.2)",
+      } as CSSProperties;
+    }
+    return {
+      // scale: 0.8,
+      transform: "scale(0.8)",
+    } as CSSProperties;
   };
 
   return (
     <>
-      <Typography variant="h6">Which do you prefer?</Typography>
-      <Grid container spacing={2} mt={2}>
+      <Typography variant="h5">De qual opção gostam mais?</Typography>
+      <Grid
+        container
+        spacing={8}
+        mt={2}
+        p={4}
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
         {[image1, image2].map((img) => (
-          <Grid key={img.value}>
-            <div onClick={() => onChange(img.value)}>
-              <Card
-                sx={{
-                  border: isPicked(img.value)
-                    ? "2px solid #1976d2"
-                    : "1px solid #ccc",
-                  borderRadius: 2,
-                }}
-              >
+          <Grid key={img.value} size={6}>
+            <div onClick={(e) => onChange(img.value)}>
+              <Card sx={getStyle(img.value)} className="option-image">
                 <CardMedia
                   component="img"
-                  height="140"
+                  height="300"
                   image={img.url}
                   alt={img.value}
                 />
