1c21f0ec8e935f1c70c356990568a646a1b6dcc9
Patrícia Silva <psilva@imaginarycloud.com>
2025-05-14 17:27:14 +0100

    add persistency on redux so on refresh data is kept

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // uses localStorage
 import quizReducer from "./quizSlice";
+
+const persistConfig = {
+  key: "patsilvarte-quiz",
+  storage,
+};
+
+const persistedQuizReducer = persistReducer(persistConfig, quizReducer);
 
 export const store = configureStore({
   reducer: {
-    quiz: quizReducer,
+    quiz: persistedQuizReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// Types for hooks
+export const persistor = persistStore(store);
+
+// types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
