dd0d7b74b483666f37c5e179d2f0506f0c6ca641
Patrícia Silva <psilva@imaginarycloud.com>
2025-05-21 00:10:11 +0100

    add no info state to user info bar on results page

--- src/reusable/UserInfo.tsx
+++ src/reusable/UserInfo.tsx
@@ -17,21 +17,52 @@ export const UserInfo = () => {
     setOpen(true);
   };
 
+  const hasNames = !!userInfo.names && userInfo.names !== "";
+  const hasDate = !!userInfo.date && userInfo.date !== "";
+  const hasColors = !!userInfo.colors && userInfo.colors?.length !== 0;
+  const hasUserInfo = !!userInfo && (hasColors || hasNames || hasDate);
+
   return (
     <>
       <div className="user-info">
-        <Typography variant="h6">
-          Casamento de <span className="highlight">{userInfo.names}</span> no
-          dia <span className="highlight">{userInfo.date}</span> com as cores
-        </Typography>
-        <MultiColorPicker colors={userInfo.colors ?? []} setColors={() => {}} />
-        <button
-          onClick={handleOpen}
-          className="link"
-          style={{ position: "absolute", right: 40 }}
-        >
-          <EditIcon />
-        </button>
+        {hasUserInfo ? (
+          <>
+            <Typography variant="h6">
+              Casamento
+              {hasNames && " de "}
+              {hasNames && <span className="highlight">{userInfo.names}</span>}
+              {hasDate && " no dia "}
+              {hasDate && <span className="highlight">{userInfo.date}</span>}
+              {hasColors && " com as cores "}
+            </Typography>
+            {hasColors && (
+              <MultiColorPicker
+                colors={userInfo.colors ?? []}
+                setColors={() => {}}
+              />
+            )}
+            <button
+              onClick={handleOpen}
+              className="link"
+              style={{ position: "absolute", right: 40 }}
+            >
+              <EditIcon />
+            </button>
+          </>
+        ) : (
+          <>
+            <Typography variant="h6">
+              Queremos saber mais sobre o vosso camsamento
+            </Typography>
+            <button
+              onClick={handleOpen}
+              className="link"
+              style={{ position: "absolute", right: 40 }}
+            >
+              Adicionar informação
+            </button>
+          </>
+        )}
       </div>
       <UserInfoModal open={open} onClose={handleClose} />
     </>
