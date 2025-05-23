d59696fd3c12318d12c2c7b4e2cc682801773c9d
Patrícia Silva <psilva@imaginarycloud.com>
2025-05-23 18:07:49 +0100

    add questions for AvsB

--- src/data/invites_data.tsx
+++ src/data/invites_data.tsx
@@ -10,7 +10,7 @@ import formatoDiferente2 from "../assets/questions/formato-diferente2.jpg";
 import formatoDiferente3 from "../assets/questions/formato-diferente3.jpg";
 import formatoDiferente4 from "../assets/questions/formato-diferente4.jpg";
 import formatoDiferente5 from "../assets/questions/formato-diferente5.jpg";
-import formatoDiferente6 from "../assets/questions/formato-diferente6.jpg";
+//import formatoDiferente6 from "../assets/questions/formato-diferente6.jpg";
 //import formatoDiferente7 from "../assets/questions/formato-diferente7.jpg";
 import lacreEnvelopeEscuro from "../assets/questions/lacre-envelope-escuro.jpg";
 import lacreTrsnaparenteFlor from "../assets/questions/lacre-transparente-flor.jpg";
@@ -26,19 +26,64 @@ import simples1 from "../assets/questions/simples1.jpg";
 //import simples2 from "../assets/questions/simples2.jpg";
 import simples3 from "../assets/questions/simples3.jpg";
 //import simples4 from "../assets/questions/simples4.jpg";
-//import simples5 from "../assets/questions/simples5.jpg";
+import simples5 from "../assets/questions/simples5.jpg";
 
 import { QAvsB, QGNNG } from "../types";
 
-const Q1: QAvsB = {
+const ABQ1: QAvsB = {
   format: "AvsB",
-  image1: { url: classico1, value: "Option A" },
+  image1: { url: simplesLacreFio, value: "Option A" },
   image2: { url: dinamico2, value: "Option B" },
 };
-const Q2: QAvsB = {
+const ABQ2: QAvsB = {
   format: "AvsB",
-  image1: { url: bilhete1, value: "Option A" },
-  image2: { url: dinamico2, value: "Option B" },
+  image1: { url: simples3, value: "Option A" },
+  image2: { url: classico1, value: "Option B" },
+};
+const ABQ3: QAvsB = {
+  format: "AvsB",
+  image1: { url: formatoDiferente4, value: "Option A" },
+  image2: { url: formatoDiferente5, value: "Option B" },
+};
+const ABQ4: QAvsB = {
+  format: "AvsB",
+  image1: { url: lacreTransparente1, value: "Option A" },
+  image2: { url: formatoDiferente1, value: "Option B" },
+};
+const ABQ5: QAvsB = {
+  format: "AvsB",
+  image1: { url: simples5, value: "Option A" },
+  image2: { url: classicoRelevoArco, value: "Option B" },
+};
+const ABQ6: QAvsB = {
+  format: "AvsB",
+  image1: { url: formatoDiferente3, value: "Option A" },
+  image2: { url: romanticoLacreFitaClaro, value: "Option B" },
+};
+const ABQ7: QAvsB = {
+  format: "AvsB",
+  image1: { url: formatoDiferente2, value: "Option A" },
+  image2: { url: relevo1, value: "Option B" },
+};
+const ABQ8: QAvsB = {
+  format: "AvsB",
+  image1: { url: simples1, value: "Option A" },
+  image2: { url: dinamico1, value: "Option B" },
+};
+const ABQ9: QAvsB = {
+  format: "AvsB",
+  image1: { url: lacreEnvelopeEscuro, value: "Option A" },
+  image2: { url: lacreTrsnaparenteFlor, value: "Option B" },
+};
+const ABQ10: QAvsB = {
+  format: "AvsB",
+  image1: { url: simples5, value: "Option A" },
+  image2: { url: recorte1, value: "Option B" },
+};
+const ABQ11: QAvsB = {
+  format: "AvsB",
+  image1: { url: passport2, value: "Option A" },
+  image2: { url: bilhete1, value: "Option B" },
 };
 
 const GNNGQ1: QGNNG = {
@@ -75,7 +120,7 @@ const GNNGQ4: QGNNG = {
   imageUrl: dinamico2,
   field: [
     { text: "Letra" },
-    { text: "Com interação" },
+    { text: "Convite dinâmico" },
     { text: "Papel translúcido" },
     { text: "Foto em destaque" },
   ],
@@ -97,6 +142,7 @@ const GNNGQ6: QGNNG = {
     { text: "Formato oval" },
     { text: "Lacre com planta" },
     { text: "Com envelope" },
+    { text: "Envelope escrito" },
   ],
 };
 const GNNGQ7: QGNNG = {
@@ -115,6 +161,7 @@ const GNNGQ8: QGNNG = {
     { text: "Formato janela" },
     { text: "Foto em destaque" },
     { text: "Faixa" },
+    { text: "Frase inspiracional" },
   ],
 };
 const GNNGQ9: QGNNG = {
@@ -124,14 +171,10 @@ const GNNGQ9: QGNNG = {
     { text: "Formato semi arco" },
     { text: "Múltiplos elementos" },
     { text: "Lacre" },
-    { text: "Faixa" },
+    { text: "Apontamento de tecido" },
   ],
 };
-const GNNGQ10: QGNNG = {
-  format: "GNNG",
-  imageUrl: formatoDiferente6,
-  field: [{ text: "Formato tudo em 1" }, { text: "Elementos florais" }],
-};
+
 const GNNGQ11: QGNNG = {
   format: "GNNG",
   imageUrl: lacreEnvelopeEscuro,
@@ -147,7 +190,7 @@ const GNNGQ12: QGNNG = {
   imageUrl: lacreTrsnaparenteFlor,
   field: [
     { text: "Lacre com flor" },
-    { text: "Papel translucido em parte" },
+    { text: "Faixa de papel translucido" },
     { text: "Sem envelope" },
     { text: "Papel irregular" },
   ],
@@ -169,7 +212,6 @@ const GNNGQ14: QGNNG = {
     { text: "Temática viagens" },
     { text: "Formato passport" },
     { text: "Elemento decoratico" },
-    { text: "Cores vivas" },
   ],
 };
 const GNNGQ15: QGNNG = {
@@ -178,34 +220,30 @@ const GNNGQ15: QGNNG = {
   field: [
     { text: "Temática viagens" },
     { text: "Formato passport" },
-    { text: "Fita" },
-    { text: "Cores suaves" },
+    { text: "Apontamento de tecido" },
+    { text: "Cores suaves" }, // talvez tirar
   ],
 };
+
 const GNNGQ16: QGNNG = {
   format: "GNNG",
   imageUrl: recorte1,
   field: [
     { text: "Formato janela" },
     { text: "Recorte decorativo" },
-    { text: "Logo casal" },
+    { text: "Emblesa iniciais" },
   ],
 };
 const GNNGQ17: QGNNG = {
   format: "GNNG",
   imageUrl: relevo1,
-  field: [
-    { text: "CEnvelope de puxar" },
-    { text: "Relevo" },
-    { text: "Letra" },
-  ],
+  field: [{ text: "Envelope de puxar" }, { text: "Relevo" }, { text: "Letra" }],
 };
 const GNNGQ18: QGNNG = {
   format: "GNNG",
   imageUrl: romanticoLacreFitaClaro,
   field: [
-    { text: "Lacre com fita" },
-    { text: "Multiplos elemetos" },
+    { text: "Lacre com tecido" },
     { text: "Papel irregular" },
     { text: "Letra" },
   ],
@@ -215,7 +253,6 @@ const GNNGQ19: QGNNG = {
   imageUrl: simplesLacreFio,
   field: [
     { text: "Formato simples" },
-    { text: "Papel translucido" },
     { text: "Lacre com fio" },
     { text: "Letra" },
   ],
@@ -224,7 +261,7 @@ const GNNGQ20: QGNNG = {
   format: "GNNG",
   imageUrl: dinamico1,
   field: [
-    { text: "Com interação" },
+    { text: "Convite dinâmico" },
     { text: "Tassel" },
     { text: "Fotos em destaque" },
     { text: "Formato" },
@@ -242,6 +279,17 @@ const GNNGQ22: QGNNG = {
 };
 
 export const quizConvites = [
+  ABQ1,
+  ABQ2,
+  ABQ3,
+  ABQ4,
+  ABQ5,
+  ABQ6,
+  ABQ7,
+  ABQ8,
+  ABQ9,
+  ABQ10,
+  ABQ11,
   GNNGQ1,
   GNNGQ2,
   GNNGQ3,
@@ -251,7 +299,6 @@ export const quizConvites = [
   GNNGQ7,
   GNNGQ8,
   GNNGQ9,
-  GNNGQ10,
   GNNGQ11,
   GNNGQ12,
   GNNGQ13,
